8ae83d971472e215db340152ab63d3f49b02fb4a
deepak <deepakb@protonmail.com>
2019-09-22 15:49:26 +0530

    Added Encypt-Decrypt functions, Updated Page Styling. Added Test code for mailer

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+//const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
+const Tesseract = require("tesseract.js");
+const { TesseractWorker } = Tesseract;
 
 
 const http = require('http').Server(app);
@@ -12,13 +16,44 @@ const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.json({ limit: '5mb' })); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true }));
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 
 
 
-const Tesseract = require("tesseract.js");
 
-const { TesseractWorker } = Tesseract;
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    secure: false,
+    auth: {
+        user: "app.readerbills@gmail.com",
+        pass: ""
+    }
+});
+
+
+function sendActivationMail(code) {
+
+    let mailOptions = {
+        from: "app.readerbills@gmail.com",
+        to: "xxx@yyy.com",
+        subject: "Your Activation Code:",
+        html: `<h2>Hello!</h2>
+        <p>Thank You for your interest in trying out Bill Reader</p>
+        <p>Your activation code is: <b>${code}</b></p> `
+    }
+
+    transporter.sendMail(mailOptions, function(err, data) {
+        if (err) {
+            console.log("Node Mailer Error: \n" + err)
+        } else {
+            console.log("Email delivered")
+        }
+    });
+}
+//sendActivationMail("234");
+
+
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
@@ -27,41 +62,56 @@ app.get("/", (req, res) => {
 
 app.post("/processimage", (req, res) => {
     console.log("Image processing...");
-    console.log(req.body.myimg);
-   // res.json({ status: "test without image success" });
-
-    scanBill(req.body.myimg).then(function(data) {
-        //console.log(data);
+    //console.log(req.body.myimg);
+    scanBill(req.body.img).then(function(data) {
+        console.log(data);
         res.json({ status: data });
-    }).catch(function(err) {
-        //console.log(err);
+    }).catch(function(err) {        
         res.json({ status: err });
     });
 
 });
 
 
+function generateEmailConstantKey(email){
+    let constantkey = "2UoH8OIQlxWJzAVcu9T6smLNXpFqZSR1tyD+g4bnwCfhkd=GKv7BMeaYirEjP503";
+    let encodedEmail = Buffer.from(email).toString('base64');
+    console.log(encodedEmail);
+    const len = encodedEmail.length;
+    const max = constantkey.length-1;  
+    let str = ``;
+    for(let i=0; i<len;i++){
+        let index = constantkey.indexOf(encodedEmail.substr(i,1)) + email.length;
+        index = index >= max ? index-max : index;
+        str = `${str}${constantkey.substr(index,1)}`;
+    }   
+    console.log(str);
+    return Buffer.from(str).toString('base64')
+}
+console.log(generateEmailConstantKey("gopinath@gmail.com"));
 
-function scanBill(img) {    
+
+function scanBill(img) {
     const worker = new TesseractWorker();
     return worker.recognize(img)
         .progress((p) => {
             //console.log('progress', p);
         })
         .then(({ text }) => {
-           // console.log(text);
-            let receiptTitle = sanitiser(text.split("\n")[0], false);
+            console.log(text);
+            let receiptTitle = sanitiser(text.split("\n")[0], false);            
             let arr = text.toLowerCase().split("\n");
+            let dateStr = dateSearch(arr);
             arr = arr.filter(function(txt) {
-                return (txt.includes("total") || txt.includes("amount") || txt.includes("payable"));
+                return (txt.includes("total") || txt.includes("amount") || txt.includes("amnt") || txt.includes("payable"));
             });
 
-           // console.log("---------------------------------------------------------");
+            
             worker.terminate();
             return new Promise((resolve, reject) => {
                 if (arr.length > 0) {
                     let get_total = sanitiser(extractTotalVal(arr), true);
-                    resolve(receiptTitle + "  " + get_total)
+                    resolve({title:receiptTitle, total:get_total, date: dateStr});
                 } else {
                     reject("No Total Found")
                 }
@@ -69,11 +119,43 @@ function scanBill(img) {
             });
 
         });
-    // counter++;
-    //scanBill();
+    
 }
 
+function dateSearch(lines){
+    let pattern1 = new RegExp("([0-9]){1,2}/([0-9]){1,2}/([0-9]){2,4}");
+    let pattern2 = new RegExp("([0-9]){1,2}-([0-9]){1,2}-([0-9]){2,4}");
+    let dates = [];
+    //console.log("date>>>>"+lines.length);
+    let monthCheck={
+        vals: function(v){
+            if(v[2].length == 2 || v[2].length == 4){
+                if(Number(v[1])>12){
+                    let formattedMonth = `${v[1]}/${v[0]}/${v[2]}`;
+                    return formattedMonth;
+                }else{
+                   return v.join("/"); 
+               }                
+            }
+            return false;  
+        }
+    };
 
+    lines.forEach(function(line){
+        
+        let l1 = line.match(pattern1);
+        let l2 = line.match(pattern2);
+        if(l1 !=null && l1.length > 1){
+            let m1 = monthCheck.vals(l1[0].split("/"));
+            if(m1){dates.push(m1)}                      
+        }
+        if(l2 !=null && l2.length > 1){
+            let m2 = monthCheck.vals(l2[0].split("/"));
+            if(m2){dates.push(m2)}           
+        }        
+    })
+    return dates[0]
+}
 
 function extractTotalVal(totals) {
     let totalValue = "";
@@ -88,6 +170,7 @@ function extractTotalVal(totals) {
                 total.indexOf("total amount") >= 0 ? total.split("total amount")[1] :
                 total.indexOf("payable") >= 0 ? total.split("payable")[1] :
                 total.indexOf("amount") >= 0 ? total.split("amount")[1] :
+                total.indexOf("amnt") >= 0 ? total.split("amnt")[1] :
                 (subs == "" && total.indexOf("total") >= 0) ? total.split("total")[1] : "";
         }
     });
@@ -100,6 +183,7 @@ function extractTotalVal(totals) {
 function sanitiser(str, isNumber) {
     let chars = isNumber ? "0123456789." : "0123456789qwertyuioplkjhgfdsazxcvbnm &QWERTYUIOPLKJHGFDSAZXCVBNM-";
     let newchar = "";
+    console.log("################# \n"+str);
     for (let i = 0; i < str.length; i++) {
         let txt = str.substr(i, 1);
         if (chars.indexOf(txt) >= 0) {
@@ -109,8 +193,14 @@ function sanitiser(str, isNumber) {
     return newchar;
 }
 
+//console.log(Buffer.from("lop@yahoo.com").toString('base64'));
+//console.log(Buffer.from("am9zZXBoQHlhaG9vLmNvbQ==","base64").toString('ascii'));
 
+
+// pattern = new RegExp(char,"g")
+// ("this is some text").replace(pattern,"0")
 
 http.listen(port, () => {
     console.log(`Server running at port ` + port);
 });
+
--- public/readImage.js
+++ public/readImage.js
@@ -16,25 +16,28 @@ fileImg.addEventListener('change', () => {
 
 function imageProcess(imgfile) {
     if (imgfile.type.indexOf("image/") > -1) {
-        let img = document.getElementById('previewimg');
+        let img = document.querySelector('.previewimg img');
+        document.querySelector('.lds-roller').classList.remove("hide");
 
         let fileReader = new FileReader();
         fileReader.onload = function(fileLoadedEvent) {
             let srcData = fileLoadedEvent.target.result; // <--- data: base64            
             console.log("Init processing....\n" + srcData);
             img.src = srcData;
-            document.getElementById("previewimg").style.display="block";
+            document.querySelector('.lds-roller').classList.remove("hide");
 
-            fetch("../processimage/", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ myimg: srcData }) })
+            fetch("../processimage/", { method: "POST", headers: { "Content-Type": "application/json" }, body: JSON.stringify({ img: srcData }) })
                 .then(data => data.json())
                 .then(function(json) {
                     console.log(json.status);
+                    document.querySelector('.previewimg').classList.remove("hide");
+                    document.querySelector(".lds-roller").classList.add("hide");
+                    displayBillingTable(json.status);
                 }).catch(function(s) {
                     console.log("fetch API failed..");
-                    console.log(s)
-
+                    console.log(s);
+                    document.querySelector(".lds-roller").classList.add("hide");
                 });
-
         }
 
         if (staticfile != "") {
@@ -54,5 +57,124 @@ function imageProcess(imgfile) {
     } else if (imgfile.type.indexOf("video/") > -1) {
         alert("Expecting an Image file")
     }
+
+}
+
+function displayBillingTable(data) {
+    document.getElementById("billTable").classList.remove("hide");
+    document.getElementById("date_field").value = data.date || "";
+    document.getElementById("merchant_field").value = data.title || "";
+    document.getElementById("amount_field").value = data.total || "";
+
+
+}
+
+function generatePasswordKey(pswd, constEmailCode) {
+
+    let totals = 1;
+    let pswdchars = ``;
+    let emChars = ``;
+    for (let j = 0; j < constEmailCode.length; j++) {
+        let char = constEmailCode.substr(j, 1);
+        emChars = `${emChars}${constEmailCode.charCodeAt(j)}`;
+        if (!isNaN(char)) {
+            totals = totals + Number(char)
+        }
+    }
+    
+    for (let i = 0; i < pswd.length; i++) {
+        let pswdCodeStr = pswd.charCodeAt(i) + totals;
+        pswdchars = `${pswdchars}${pswdCodeStr}`;
+    }
+    
+    let min = Math.min(emChars.length, pswdchars.length);
+    let loopString = emChars.length == min ? emChars : pswdchars;
+    let compareString = emChars.length == min ? pswdchars : emChars;
+    let result = ``;
+
+    for (let q = 0; q < min; q++) {
+        let val = Math.abs(Number(loopString.substr(q, 1)) - Number(compareString.substr(q, 1)));
+        result = `${result}${val}`;
+    }
+
+    let encoded = btoa(result);
+    let half = Math.ceil(encoded.length / 2);
+    return {
+        serv: encoded.substr(0, half),
+        cli: encoded.substr(half)
+    }
+
+}
+
+
+
+function encrypt(_imgdata, enc_key) {
+    let cryptoVals = getCryptVals(enc_key);
+    let img_data = _imgdata.split(";base64,");
+    let imgdata = img_data[1];
+    let imgBlocks = Math.ceil(imgdata.length / cryptoVals.divBlocks);
+    /*console.log(imgBlocks);*/
+    let imgBlockList = [];
+    for (let b = 0; b < cryptoVals.divBlocks; b++) {
+        let cutPoint = b * imgBlocks;
+        imgBlockList.push(imgdata.substr(cutPoint, imgBlocks));
+    }
+    /*console.log(imgBlockList);*/
+
+
+    let extractedBlocks = [];
+    let encrypted = ``;
+    imgBlockList.forEach(function(item) {
+        let substr = item.substr(cryptoVals.position, cryptoVals.totalExtr);
+        let parts = item.split(substr);
+        encrypted = `${encrypted}${parts[0]}${parts[1]}${cryptoVals.partition}`;
+        extractedBlocks.push(substr);
+    });
+    let base = `${img_data[0]};base64,`;
+    return `${encrypted}${extractedBlocks.join("")}__${btoa(base)}`;
+}
+
+function getCryptVals(encr) {
+    let enckey = `${encr.serv}${encr.cli}`;
+    let cli_key = encr.cli;
+    let chars = "zxcvbnmlkjhgfdsaqwertyiopuQWERTYUIOPLKJHGFDSAZXCVBNM0123456789+=/";
+    let totalextract = 0;
+
+    // get Uniques from enckey;
+    let allchars = [...enckey.split(""), ...chars.split("")];
+    let uniqueKey = new Set(allchars);
+    let uKey = [...uniqueKey];
+    console.log("Unique key:" + uKey.join(""));
+    for (let i = 0; i < cli_key.length; i++) {
+        totalextract = totalextract + uKey.indexOf(cli_key.substr(i, 1));
+    }
+    /*console.log("total to extract:" + totalextract);*/
+    let minDivideBlocks = uKey.indexOf(cli_key.substr(0, 1));
+    minDivideBlocks = minDivideBlocks > 30 ? 30 : minDivideBlocks < 5 ? 5 : minDivideBlocks;
+    let extractPos = uKey.indexOf(cli_key.substr(1, 1));
+    /*console.log("blocks:" + minDivideBlocks + " = " + cli_key.substr(0, 1));
+    console.log("Extr Pos:" + extractPos + " = " + cli_key.substr(1, 1));
+*/
+
+    let fromClient = cli_key.substr(-3);
+    let partitionStr = `${fromClient.charCodeAt(0)}${fromClient.charCodeAt(1)}${fromClient.charCodeAt(2)}`;
+    partitionStr = btoa(partitionStr);
+
+
+    return { totalExtr: totalextract, divBlocks: minDivideBlocks, position: extractPos, partition: partitionStr }
+}
+
+function decrypt(encryptedImg, enc_key) {
+    let cryptoVals = getCryptVals(enc_key);
+    let splitter = encryptedImg.split("__");
+    let base = atob(splitter[1]);
+    let blocks = splitter[0].split(cryptoVals.partition);
+    let extractedSeq = blocks.pop();
+    let decrypted = ``;
+    blocks.forEach(function(part, i) {
+        let extract = extractedSeq.substr(i * cryptoVals.totalExtr, cryptoVals.totalExtr);
+        decrypted = `${decrypted}${part.substr(0,cryptoVals.position)}${extract}${part.substr(cryptoVals.position)}`
+    })
+    return `${base}${decrypted}`;
 
 }
