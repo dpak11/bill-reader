ef8397c21fa224dcd27778c118d400761fd4a690
deepak <deepakb@protonmail.com>
2019-09-24 17:04:42 +0530

    corrected environment variables naming

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 
 
 if(process.env.MONGO_URL && process.env.APP_MAILER_PSWD){
-	module.exports = {mongodb: process.env.MONGO_URL,mailerPswd:process.env.APP_MAILER_PSWD,email:APP_MAILER_email}
+	module.exports = {mongodb: process.env.MONGO_URL,mailerPswd:process.env.APP_MAILER_PSWD,email:process.env.APP_MAILER_EMAIL}
 }else{	
 	module.exports = require("./dev");
 }
