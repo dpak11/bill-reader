61a43ef595e25efbd3f738e6d8abca81a2ecb1ef
deepak <deepakb@protonmail.com>
2019-10-22 20:33:50 +0530

    added 2 new categories for bills - personal account

--- public/pwa.js
+++ public/pwa.js
@@ -3,5 +3,5 @@ if('serviceWorker' in navigator){
     .then(reg => console.log('service worker registered'))
     .catch(err => console.log('service worker not registered', err));
 }else{
-	alert("Your browser does not support some feature.\nPlease upgrade to the latest Google Chrome or Firefox browser")
+	alert("Your browser does not support some features.\nThis is either because you are not on the latest version of Google Chrome/Firefox.\nOr you are currently in Incognito/Private Mode")
 }
